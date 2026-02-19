a4ca789e2284fdce7fe9a7a9de657a535867352d
Divya <divya@example.com>
2026-02-19 16:56:59 +0530

    fix: exhaustive force dynamic behavior on all API routes

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,6 +2,8 @@ import bcrypt from "bcryptjs";
 import prisma from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request: Request) {
     try {
         const body = await request.json();
